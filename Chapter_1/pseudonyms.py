b27909b52965294e43074ce5496ce634564b8cf1
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-12-03 17:03:00 +0900

    space cut

--- Chapter_1/pseudonyms.py
+++ Chapter_1/pseudonyms.py
@@ -36,15 +36,11 @@ last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
         'Woolysocks')
 
 while True:
-
     firstName = random.choice(first)
-
     lastName = random.choice(last)
-
     print("\n\n")
     print(firstName, lastName, file = sys.stderr)
     print("\n\n")
-
     try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
     if try_again.lower() == "n":
         break
